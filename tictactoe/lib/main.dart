f0b20d1155a164fd7bbec32bb71b9a4fd9e1e04c
Peter-Xidian <mayowaodebode@gmail.com>
2021-06-22 17:14:19 +0200

    first commit

---
+++ tictactoe/lib/main.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool ohTurn = true; //The first player is O!
+  List<String> displayExOh = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TicTacToe - Lets Play!"),
+        backgroundColor: Colors.green.shade300,
+      ),
+      backgroundColor: Colors.blueGrey.shade900,
+      body: GridView.builder(
+          itemCount: 9,
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () => _tapped(index),
+              child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green.shade100)),
+                  child: Center(
+                    child: Text(
+                      displayExOh[index],
+                      style: TextStyle(
+                          color: Colors.blueGrey.shade100, fontSize: 80),
+                    ),
+                  )),
+            );
+          }),
+    );
+  }
+
+  void _tapped(int index) {
+    setState(() {
+      if (ohTurn) {
+        displayExOh[index] = 'o';
+      } else {
+        displayExOh[index] = 'x';
+      }
+
+      ohTurn = !ohTurn;
+      _checkWinner();
+    });
+  }
+
+  void _checkWinner() {
+    // checks 1st row
+    if (displayExOh[0] == displayExOh[1] &&
+        displayExOh[0] == displayExOh[2] &&
+        displayExOh[0] != '') {
+      _showWinDialog();
+    }
+
+    // checks 2nd row
+    if (displayExOh[3] == displayExOh[4] &&
+        displayExOh[3] == displayExOh[5] &&
+        displayExOh[3] != '') {
+      _showWinDialog();
+    }
+
+    // checks 3rd row
+    if (displayExOh[6] == displayExOh[7] &&
+        displayExOh[6] == displayExOh[8] &&
+        displayExOh[6] != '') {
+      _showWinDialog();
+    }
+
+    // checks 1st column
+    if (displayExOh[0] == displayExOh[3] &&
+        displayExOh[0] == displayExOh[6] &&
+        displayExOh[0] != '') {
+      _showWinDialog();
+    }
+
+    // checks 2nd column
+    if (displayExOh[1] == displayExOh[4] &&
+        displayExOh[1] == displayExOh[7] &&
+        displayExOh[1] != '') {
+      _showWinDialog();
+    }
+
+    // checks 3rd column
+    if (displayExOh[2] == displayExOh[5] &&
+        displayExOh[2] == displayExOh[8] &&
+        displayExOh[2] != '') {
+      _showWinDialog();
+    }
+
+    // checks diagonal
+    if (displayExOh[6] == displayExOh[4] &&
+        displayExOh[6] == displayExOh[2] &&
+        displayExOh[6] != '') {
+      _showWinDialog();
+    }
+
+    // checks diagonal
+    if (displayExOh[0] == displayExOh[4] &&
+        displayExOh[0] == displayExOh[8] &&
+        displayExOh[0] != '') {
+      _showWinDialog();
+    }
+  }
+
+  void _showWinDialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Winner!'),
+          );
+        });
+  } //
+
+}
